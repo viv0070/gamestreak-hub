959dcb77653132f4be86922a8a9f32408d2013a3
viv0070 <dsvivek8612@gmail.com>
2025-06-13 19:35:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Future feature: alert when "Buy Now" is clicked
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        alert("Redirecting to payment gateway... (feature under development)");
+    });
+});
